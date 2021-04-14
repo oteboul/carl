084e45854fe16433b0be179fa55d30bd726662d9
Mathïs Fédérico <mathfederico@gmail.com>
2021-04-14 17:41:09 +0200

    Updated tournament_dqn

--- carl/tournaments/tournament_dqn.py
+++ carl/tournaments/tournament_dqn.py
@@ -23,34 +23,32 @@ class MultiAgent(Agent):
             return self.agents[0].act(observations, greedy)
 
 circuits = [
-    # [(0.5, 0), (2.5, 0), (3, 1), (3, 2), (2, 3), (1, 3), (0, 2), (0, 1)],
+    [(0.5, 0), (2.5, 0), (3, 1), (3, 2), (2, 3), (1, 3), (0, 2), (0, 1)],
     # [(0, 0), (1, 2), (0, 4), (3, 4), (2, 2), (3, 0)],
     # [(0, 0), (0.5, 1), (0, 2), (2, 2), (3, 1), (6, 2), (6, 0)],
     # [(1, 0), (6, 0), (6, 1), (5, 1), (5, 2), (6, 2), (6, 3),
     #  (4, 3), (4, 2), (2, 2), (2, 3), (0, 3), (0, 1)],
     # [(2, 0), (5, 0), (5.5, 1.5), (7, 2), (7, 4), (6, 4), (5, 3), (4, 4),
     #  (3.5, 3), (3, 4), (2, 3), (1, 4), (0, 4), (0, 2), (1.5, 1.5)],
-    generate_circuit(n_points=50, difficulty=0),
-    generate_circuit(n_points=20, difficulty=8),
-    generate_circuit(n_points=20, difficulty=16),
-    generate_circuit(n_points=20, difficulty=32),
-    generate_circuit(n_points=30, difficulty=32),
+    # generate_circuit(n_points=50, difficulty=0),
+    # generate_circuit(n_points=20, difficulty=8),
+    # generate_circuit(n_points=20, difficulty=16),
+    # generate_circuit(n_points=20, difficulty=32),
+    # generate_circuit(n_points=30, difficulty=32),
 ]
 
-# filenames = os.listdir(os.path.join('models', 'DQN', 'test_done'))
-filenames = ['Beholder10.h5']
+filenames = os.listdir(os.path.join('models', 'DQN'))
 teamnames = [name.split('.')[0].capitalize() for name in filenames]
-
 n_agents = len(filenames)
+
 env = Environment(circuits, n_agents, names=teamnames,
     action_type='discrete', n_sensors=7, fov=np.pi*210/180)
 
 agents = [DQNAgent(env.action_space) for _ in range(n_agents)]
-
 for agent, filename in zip(agents, filenames):
     filepath = os.path.join('models', 'DQN', filename)
     agent.load(filepath)
 
 multi_agent = MultiAgent(agents)
-pg = Playground(env, multi_agent)
-pg.test(3 * len(circuits), callbacks=[ScoreCallback()], verbose=2)
+playground = Playground(env, multi_agent)
+playground.test(len(circuits), callbacks=[ScoreCallback()])
